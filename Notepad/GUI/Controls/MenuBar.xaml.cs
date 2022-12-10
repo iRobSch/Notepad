011b8f57cc717bb9144e95c5d05b3c642352edef
Robin Schenkels <unconfigured@null.spigotmc.org>
2022-12-10 17:22:50 +0100

    Add method to (temporarily) disable certain items in the menu bar

--- Notepad/GUI/Controls/MenuBar.xaml.cs
+++ Notepad/GUI/Controls/MenuBar.xaml.cs
@@ -1,12 +1,33 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using Microsoft.UI.Xaml.Controls;
+
 namespace Notepad.GUI.Controls;
 
 public sealed partial class MenuBar
 {
+    private readonly List<MenuFlyoutItem> _disabledItems;
+
     public MenuBar()
     {
         this.InitializeComponent();
+
+        _disabledItems = new List<MenuFlyoutItem>
+        {
+            RedoItem, UndoItem, CutItem, CopyItem, PasteItem, DeleteItem, SelectItem, 
+            ReadItem, FindItem, FindNextItem, FindPrevItem, ReplaceItem, PreferencesItem, ImportItem
+        };
+
+        this.DisableItems();
+    }
+
+    private void DisableItems()
+    {
+        foreach (MenuFlyoutItem item in _disabledItems)
+        {
+            item.IsEnabled = false;
+        }
     }
 }
