f8b37b26de8e39f5865f9245e14f316d6c4dec72
Robin Schenkels <unconfigured@null.spigotmc.org>
2022-12-10 21:57:59 +0100

    Add basic methods for reading and writing to a text file

--- Notepad/Actions/FileActions.cs
+++ Notepad/Actions/FileActions.cs
@@ -1,6 +1,29 @@
-﻿namespace Notepad.Actions;
+﻿using System.IO;
+
+namespace Notepad.Actions;
 
 internal class FileActions
 {
+    public static GUI.Controls.Notepad Current = GUI.Controls.Notepad.Current;
 
+    /// <summary>
+    /// Writes the content of the main text box to a StreamWriter object.
+    /// </summary>
+    private void WriteToFile()
+    {
+        StreamWriter writer = new("Notepad");
+        writer.Write(Current.TextBox.Text);
+        writer.Close();
+    }
+
+    /// <summary>
+    /// Reads the content of a text file, and writes it to the main text box.
+    /// </summary>
+    /// <param name="file">The file that will be read from.</param>
+    public void ReadFromFile(string file)
+    {
+        StreamReader reader = new(file);
+        Current.TextBox.Text = reader.ReadToEnd();
+        reader.Close();
+    }
 }
