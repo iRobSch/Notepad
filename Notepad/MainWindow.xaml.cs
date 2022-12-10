c0d1189903ecc3dacd6c1dcad0993713d46affe4
Robin Schenkels <unconfigured@null.spigotmc.org>
2022-12-10 14:58:36 +0100

    Add future support for the titlebar and safeguards on start-up   * Added safeguards to view for active instances of the program.   * Added a default titlebar that is yet to display anything.   * Set the main window's title for the titlebar.   * Adjusted the class' main comment.

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -1,22 +1,31 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using System;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 
 namespace Notepad;
 
 /// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
+/// Displays the main window, containing various controls to work with.
 /// </summary>
-public sealed partial class MainWindow : Window
+public sealed partial class MainWindow
 {
+    public new static MainWindow Current { get; private set; }
+
     public MainWindow()
     {
-        this.InitializeComponent();
-    }
+        if (Current != null) throw new ArgumentException("An instance of MainWindow already exists.");
+        Current = this;
 
-    private void myButton_Click(object sender, RoutedEventArgs e)
-    {
-        myButton.Content = "Clicked";
+        this.InitializeComponent();
+
+        this.Title = "Notepad";
+
+        // TODO
+        //AppWindowTitleBar titlebar = AppWindow.TitleBar;
+        //titlebar.ExtendsContentIntoTitleBar = true;
+        //this.SetTitleBar(<>);
     }
 }
